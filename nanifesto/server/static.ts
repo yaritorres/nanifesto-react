51aea9f2946a8de3b96d7e2019c2ac13abba9c16
Yari Torres Nicola <ybtorres9@gmail.com>
2024-10-22 19:12:23 -0400

    server changes

--- nanifesto/server/static.ts
+++ nanifesto/server/static.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import path from 'path';
 const app = express();
+const __dirname = path.resolve();
 
 app.use(express.static(path.join(__dirname, 'build')));
 
@@ -8,4 +9,5 @@ app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
+console.log('Listening on port 9000');
 app.listen(9000);
