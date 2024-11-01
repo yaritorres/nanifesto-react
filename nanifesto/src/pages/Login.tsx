f6dfe8dbf0c470c2f77418e7035d4c4e1c5e98e3
Yari Torres Nicola <ybtorres9@gmail.com>
2024-10-31 22:31:29 -0400

    route update

--- nanifesto/src/pages/Login.tsx
+++ nanifesto/src/pages/Login.tsx
@@ -5,8 +5,6 @@ import LoginAlert from "../components/LoginAlert.tsx";
 import { useState, useEffect } from 'react';
 import { useNavigate } from "react-router-dom";
 
-axios.defaults.baseURL = 'http://ec2-13-57-35-52.us-west-1.compute.amazonaws.com:4000';
-
 export default function Login() {
   const [isLoggingIn, setIsLoggingIn] = useState<boolean>(false);
   const [failedLogin, setFailedLogin] = useState<boolean>(false);
@@ -20,7 +18,7 @@ export default function Login() {
 
   const handleLogin = () => {
     const options = {
-      url: 'http://ec2-13-57-35-52.us-west-1.compute.amazonaws.com:4000/users/login',
+      url: 'http:localhost:4000/users/login',
       headers: {}
     };
 
@@ -41,7 +39,7 @@ export default function Login() {
 
   const handleGuestLogin = () => {
     const options = {
-      url: 'http://ec2-13-57-35-52.us-west-1.compute.amazonaws.com:4000/users/login',
+      url: 'http:localhost:4000/users/login',
       headers: {}
     };
 
