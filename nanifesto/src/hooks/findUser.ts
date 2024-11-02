fdc971cf805441dca14fa1fab5cd20c6c8971099
Yari Torres Nicola <ybtorres9@gmail.com>
2024-11-01 21:15:35 -0400

    fixing url bug

--- nanifesto/src/hooks/findUser.ts
+++ nanifesto/src/hooks/findUser.ts
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://ec2-13-57-35-52.us-west-1.compute.amazonaws.com:3001';
+// axios.defaults.baseURL = '';
 
 export default async function findUser(token:string) {
   try {
     let result;
 
-    result = await axios.get('/find-user', {headers: {"Authorization": `Bearer ${token}`}});
+    result = await axios.get('http://ec2-13-57-35-52.us-west-1.compute.amazonaws.com:3001/find-user', {headers: {"Authorization": `Bearer ${token}`}});
 
     if (!result.data.username) {
       return {
